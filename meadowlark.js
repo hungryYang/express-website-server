93fc119873a906a80cd71667c2f15b3e2bc67fd9
hungryyang <ycr1714336458@163.com>
2018-02-02 15:48:15 +0800

    表单处理

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser')
 const fortune = require('./lib/fortune');
 const weather = require('./lib/getWeather')
+const formidable = require('formidable')
 const handlebars = require('express-handlebars').create({
   defaultLayout:'main',
   helpers: {
@@ -24,6 +25,7 @@ app.set('view engine', 'handlebars');
 */
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser())
 /*
 *测试页面
 */
@@ -67,7 +69,7 @@ app.get('/tours/oregon-coast', (req, res) => {
 
 app.get('/nursery-rhyme',(req,res)=>{
   res.render('nursery-rhyme')
-})
+});
 
 app.get('/data/nursery-rhyme',(req,res)=>{
   res.json({
@@ -78,6 +80,52 @@ app.get('/data/nursery-rhyme',(req,res)=>{
   });
 });
 
+app.get('/newsletter',(req,res)=>{
+  res.render('newsletter',{csrf:'CSRF token goes here'});
+});
+
+/*
+* AJAX 处理
+*/
+// app.post('/process',(req,res)=>{
+//   console.log(`Form (from querystring): ${req.query.form}`);
+//   console.log(`CSRF (from hidden form field): ${req.body._csrf}`);
+//   console.log(`Name (from hidden form field): ${req.body.name}`);
+//   console.log(`Email (from visible form field): ${req.body.email}`);
+//   res.redirect(303,'/thank-you')
+// })
+
+app.get('/thank-you', function(req, res){
+  res.render('thank-you');
+});
+
+app.post('/process',(req,res)=>{
+  if(req.xhr||req.accepts('json,html') ==='json'){
+    res.send({success:true})
+  }else{
+    res.redirect(303,'/thank-you')
+  }
+})
+/*
+* 文件上传
+*/
+app.get('/contest/vacation-photo',(req,res)=>{
+  let now = new Date();
+  res.render('contest/vacation-photo',{
+    year:now.getFullYear(),
+    month:now.getMonth()
+  })
+});
+
+app.post('/contest/vacation-photo/:year/:month',(req,res)=>{
+  let form = new formidable.IncomingForm();
+  form.parse(req,(err,fields,files)=>{
+    if(err) return res.redirect(303,'/error')
+    console.log(fields)
+    console.log(files)
+    res.redirect(303,'/thank-you')
+  })
+})
 /*
 *   错误页面
 *   如果把中间件放在所有的路由上面，那么其他URL访问的都是404
